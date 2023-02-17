9c64a49071ee1e55f939aada718181c9a6748661
Franklin Briones <franklindbriones@gmail.com>
2023-02-16 22:54:18 -0600

    work on the link component for client side navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 function Home() {
     return (
-
-    <h1>Home Page</h1>
+        <div>
+            <h1>Home Page</h1>
+            <Link href="/blog">Blog</Link>
+            <Link href="/product">Products</Link>
+        </div>
+    
     )
 }
 
