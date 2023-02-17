31e4f55a07d93bf5f02cda2022b7434f580ad4ba
Franklin Briones <franklin.briones@franklins-mbp.lan>
2023-02-17 16:45:04 -0600

    work on SSG with getstaticpaths to inform the app about possible pages to generate

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,9 @@ function Home() {
     return (
         <div>
             <h1>Next JS Pre-rendering</h1>
-            <Link href='/users'>Users</Link>
+            <Link href='/posts'>Posts</Link>
         </div>
+
     
     )
 }
