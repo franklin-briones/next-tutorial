474dd8a9659bb2e12c951c31ffb5719fe75b870c
Franklin Briones <franklin.briones@Franklins-MacBook-Pro.local>
2023-02-17 14:57:58 -0600

    work on static side generation fundamentals

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
+import Link from "next/link"
 
 function Home() {
     return (
         <div>
             <h1>Next JS Pre-rendering</h1>
+            <Link href='/users'>Users</Link>
         </div>
     
     )
