738de2767dd9f951795d616622144cf8048b8092
Franklin Briones <franklin.briones@franklins-mbp.lan>
2023-02-19 16:53:34 -0600

    fallback blocking for web crawlers and no loading bar

--- src/pages/posts/[postId].js
+++ src/pages/posts/[postId].js
@@ -1,11 +1,4 @@
-import { useRouter } from "next/router"
-
 export default function Post({ post }) {
-    const router = useRouter()
-
-    if (router.isFallback) {
-        return <h1>Loading...</h1>
-    }
     return (
         <>
             <h2>{post.id} {post.title}</h2>
@@ -46,6 +39,6 @@ export async function getStaticPaths() {
 
     return {
         paths: paths.slice(0,3),
-        fallback: true
+        fallback: 'blocking'
     }
 }
