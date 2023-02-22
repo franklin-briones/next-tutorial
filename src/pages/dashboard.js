1721a8e8d23cab0ec2efef6b25834c2a41a9c46e
Franklin Briones <franklin.briones@franklins-mbp.lan>
2023-02-21 22:10:57 -0600

    practice client side datafetching

---
+++ src/pages/dashboard.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function Dashboard() {
+    const [isLoading, setIsLoading] = useState(true)
+    const [dashboardData, setDashboardData] = useState(null)
+
+    useEffect(() => {
+        async function fetchDashboardData() {
+            const response = await fetch('http://localhost:4000/dashboard')
+            const data = await response.json()
+            setDashboardData(data)
+            setIsLoading(false)
+        }
+        fetchDashboardData()
+    }, [])
+
+    if (isLoading) {
+        return <h2>Loading</h2>
+    }
+    
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <h2>Posts - {dashboardData.posts}</h2>
+            <h2>likes - {dashboardData.likes}</h2>
+            <h2>followers - {dashboardData.followers}</h2>
+            <h2>following - {dashboardData.following}</h2>
+        </div>
+    )
+}
