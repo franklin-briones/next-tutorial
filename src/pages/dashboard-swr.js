580164bccb6d7715dbdcf1404db778c1cdcf6171
Franklin Briones <franklindbriones@gmail.com>
2023-02-21 22:22:31 -0600

    practice client side datafetching

---
+++ src/pages/dashboard-swr.js
@@ -0,0 +1,24 @@
+import useSWR from 'swr'
+
+const fetcher = async () => {
+    const response = await fetch('http://localhost:4000/dashboard')
+    const data = await response.json()
+    return data
+}
+
+export default function DashboardSWR() {
+    const { data, error } = useSWR('dashboard', fetcher)
+
+    if (error) return 'An error has occurred'
+    if (!data) return 'Loading'
+
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <h2>Posts - {data.posts}</h2>
+            <h2>likes - {data.likes}</h2>
+            <h2>followers - {data.followers}</h2>
+            <h2>following - {data.following}</h2>
+        </div>
+    )
+}
