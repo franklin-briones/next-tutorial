5a6f265d6a909476afdd937e441531e8570a58cb
Franklin Briones <franklindbriones@gmail.com>
2023-02-26 16:47:09 -0600

    client side data fecthing and pre-rendering

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,13 +1,16 @@
 import { useState } from "react"
+import { useRouter } from "next/router"
 
 export default function EventList({ eventList }) {
 
     const [events, setEvents] = useState(eventList)
+    const router = useRouter()
 
     const fetchSportsEvents = async() => {
         const res = await fetch('http://localhost:4000/events?category=sports')
         const data = await res.json()
         setEvents(data)
+        router.push('/events?category=sports', undefined, {shallow: true})
     }
 
     return (
@@ -30,8 +33,11 @@ export default function EventList({ eventList }) {
 
 
 
-export async function getServerSideProps() {
-    const res = await fetch('http://localhost:4000/events')
+export async function getServerSideProps(context) {
+    const { query } = context
+    const { category  } = query
+    const queryString = category ? 'category=sports' : ''
+    const res = await fetch(`http://localhost:4000/events${queryString}`)
     const data = await res.json()
     
     return {
